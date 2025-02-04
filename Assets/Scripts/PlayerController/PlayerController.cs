35ab433e160f1ccc3188fc9b13bb1e0f2e3fcaed
Rykaon <jimmy.fromonot@gmail.com>
2025-02-04 15:35:47 +0100

    Setup Player Archi

---
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] PlayerMovement Movement;
+    [SerializeField] PlayerGrab Grab;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
