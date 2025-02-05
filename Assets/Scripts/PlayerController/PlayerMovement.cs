53c73f03b9826533949e790088a0ba5a5e827824
ThéoMoreau <entropy.Tmoreau>
2025-02-05 11:04:53 +0100

    Import Tmp and finish movement Player

--- Assets/Scripts/PlayerController/PlayerMovement.cs
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -4,20 +4,29 @@ using System.Linq;
 using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
+using UnityEngine.Windows;
+using UnityEngine.XR.Interaction.Toolkit.Inputs;
 public class PlayerMovement : PlayerComponent
 {
-    public Vector3 velocityPlayer;
-    public bool isClimbing;
-    public Rigidbody body;
-    public InputActionProperty leftHandVelocity;
-    public InputActionProperty rightHandVelocity;
-    public float speedMultiplier;
+    private Vector3 velocityPlayer;
+    [HideInInspector] public bool isClimbing;
+    private Rigidbody body;
+    [SerializeField] private InputActionProperty leftHandVelocity;
+    [SerializeField] private InputActionProperty rightHandVelocity;
+    [SerializeField] private InputActionProperty turnCamera;
+    [SerializeField] private float speedMultiplier;
     private string handUsed;
     public Transform playerCamera;
+    public TextMeshPro DebugText;
+    private int numberOfTurn;
+
+
+    
     void Start()
     {
         body = GetComponent<Rigidbody>();
+        turnCamera.action.started += ctx => GetDirection(ctx.ReadValue<Vector2>());
     }
 
     public override void InitializeComponent(PlayerController controller)
@@ -45,11 +54,33 @@ public class PlayerMovement : PlayerComponent
 
         if (isClimbing)
         {
-          
+            velocityPlayer = Quaternion.AngleAxis(45*numberOfTurn, Vector3.up) * velocityPlayer;
             body.velocity = -velocityPlayer*speedMultiplier;
+            DebugText.text = (-velocityPlayer).ToString();
         }
     }
 
+    private void GetDirection(Vector2 inputF)
+    {
+        var cardinal = CardinalUtility.GetNearestCardinal(inputF);
+        switch (cardinal)
+        {
+            case Cardinal.North:
+                break;
+            case Cardinal.South:
+                numberOfTurn += 4;
+                break;
+            case Cardinal.East:
+                numberOfTurn++;
+                break;
+                
+            case Cardinal.West:
+                numberOfTurn--;
+
+                break;
+              
+        }
+    }
 
     public void StartClimb(string hand)
     {
